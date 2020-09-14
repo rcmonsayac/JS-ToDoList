86f831d1e89003377297cf8a9887d273621fc2d3
rcmonsayac <35493715+rcmonsayac@users.noreply.github.com>
2020-09-14 18:59:50 +0800

    Final changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 const addTodo = () =>{
     let input = document.querySelector(".input-todo");
@@ -13,13 +13,13 @@ const addTodo = () =>{
             todoContent.classList.add("_button", "todo-content");
             todoContent.dataset.action = "cross";
             todoContent.innerHTML = inputToDo;
-            todoContent.addEventListener("click", crossTodo);
+            todoContent.addEventListener("click", doAction);
 
-        let xButton = document.createElement("span");
+        let xButton = document.createElement("button");
             xButton.classList.add("_button", "rmv-btn");
             xButton.dataset.action = "remove";
             xButton.innerHTML = "<i class=\"fa fa-times\" />";
-            xButton.addEventListener("click", removeTodo);
+            xButton.addEventListener("click", doAction);
     
         todoDiv.appendChild(todoContent);
         todoDiv.appendChild(xButton);
@@ -30,19 +30,32 @@ const addTodo = () =>{
 };
 
 
-const crossTodo = function() {
-    this.classList.add("todo-crossed");
-    this.dataset.action = "";
+const crossTodo = (button) => {
+    button.classList.add("todo-crossed");
+    button.dataset.action = "";
 }
 
-const removeTodo = function() {
-    this.parentElement.remove();
+const removeTodo = function(button) {
+    button.parentElement.remove();
 }
 
+const doAction = (e) => {
+    let button = e.currentTarget;
+    let action = button.dataset.action;
+    if(action === "add"){
+        addTodo();
+    }
+    else if(action === "cross"){
+        crossTodo(button);
+    }
+    else if(action === "remove"){
+        removeTodo(button);
+    }
+}
 
 let currentDate = document.querySelector(".current-date");
 currentDate.innerHTML = new Date().toLocaleDateString();
 
 
 let addButton = document.querySelector(".add-btn");
-addButton.onclick = addTodo;
+addButton.onclick = doAction;
